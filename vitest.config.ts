39ae111de81e137c0a1713e8d51d56979faa336f
Mohammadhassan <bagheri.mh69@gmail.com>
2025-08-05 18:01:38 +0200

    conf: Add config for testing and coverage test

---
+++ vitest.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        '**/*.d.ts',
+        '**/*.config.*',
+        '**/coverage/**',
+        'dist/',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, 'src/common/components'),
+      '@constants': resolve(__dirname, 'src/common/constants'),
+      '@models': resolve(__dirname, 'src/common/models'),
+      '@hooks': resolve(__dirname, 'src/common/hooks'),
+      '@utils': resolve(__dirname, 'src/common/utils'),
+      '@styles': resolve(__dirname, 'src/common/styles'),
+    },
+  },
+})
